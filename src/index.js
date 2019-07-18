92943a14f1bf33936f3565cdfb39b0b368398605
shamin.meerankutty <shamin.meerankutty@fullcontact.com>
2019-07-18 13:58:18 +0530

    Opening login page and typing username and password

--- src/index.js
+++ src/index.js
@@ -1,10 +1,12 @@
-const puppeteer = require('puppeteer-core');
+const puppeteer = require('puppeteer');
 
 (async () => {
-  const browser = await puppeteer.launch();
+  const browser = await puppeteer.launch({headless: false, args: ['--no-sandbox']});
   const page = await browser.newPage();
-  await page.goto('https://google.com');
-  await page.screenshot({path: 'example.png'});
-
+  await page.goto('https://www.instagram.com/accounts/login');
+  await page.waitFor(5000);
+  await page.type('input[type=\"text\"]', 'Hello');
+  await page.type('input[type=\"password\"]', 'Hello');
+  await page.waitFor(2500);
   await browser.close();
 })();
